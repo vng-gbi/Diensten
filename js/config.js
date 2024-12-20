7afbb8db0a8dcbc69265522b2f84ce5efd499bbd
janbrin <jan.brinkkemper@vng.nl>
2024-12-20 14:08:46 +0100

    a

--- js/config.js
+++ js/config.js
@@ -18,7 +18,7 @@ let respecConfig = {
   //previousPublishDate: "2022-06-24",
   //previousPublishVersion: "0.0.4",
   
-  title: "Reden aanvraag",
+  title: "Diensten",
 //  subtitle: "Tevens een voorbeeld van een gegenereerd Respec document",
 //  content: {"mermaid": "", "ch01": "informative", "ch02": ""},
 //  authors:
